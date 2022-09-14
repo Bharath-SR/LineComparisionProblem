47e08f055f7b0be1c0f3cd4e42fb715569f9f473
Bharath-SR <bharath.s0941@gmail.com>
2022-09-14 21:00:01 +0530

    Added: LineComparision Problem UC1

--- LineComparision/src/com/bridgeLabz/LineComparision.java
+++ LineComparision/src/com/bridgeLabz/LineComparision.java
@@ -3,6 +3,8 @@
  */
 package com.bridgeLabz;
 
+import java.util.Scanner;
+
 /**
  * @author Admin
  *
@@ -10,10 +12,23 @@ package com.bridgeLabz;
 public class LineComparision {
 
 	/**
-	 * @param args
+	 * I want to model a line based on a point consisting of (x, y) co -ordinatesusing the Cartesian system,So that I can calculate its length
+	 *- A Length as 2 Points (x1, y1) and (x2, y2) - Length of a Line = sqrt( (x2 - x1) ^ 2 + (y2 - y1) ^ 2)
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Driving Class
+		
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the value x1");
+		int x1 =input.nextInt();
+		System.out.println("Enter the value x2");
+		int x2 =input.nextInt();
+		System.out.println("Enter the value y1");
+		int y1 =input.nextInt();
+		System.out.println("Enter the value y2");
+		int y2 =input.nextInt();
+		double distance = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+		System.out.println(distance);
 
 	}
 
